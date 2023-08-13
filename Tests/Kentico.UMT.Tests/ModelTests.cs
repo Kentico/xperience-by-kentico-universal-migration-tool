5939c56748ecc071679500e796eff1f496e806d2
Tomáš Krch <tomas@krch.tech>
2023-08-14 00:30:56 +0200

    WIP unit tests

---
+++ Tests/Kentico.UMT.Tests/ModelTests.cs
@@ -0,0 +1,107 @@
+﻿//using System.Text.Json;
+//using CMS.DataEngine;
+//using CMS.DocumentEngine;
+
+using System.Text.Json;
+using CMS.DataEngine;
+using CMS.DocumentEngine;
+using FluentAssertions;
+using Kentico.Xperience.UMT.Model;
+using Kentico.Xperience.UMT.Services;
+using Microsoft.Extensions.DependencyInjection;
+// using Kentico.Xperience.UMT.Model;
+// using Kentico.Xperience.UMT.Services;
+// using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace Kentico.Xperience.UMT;
+
+internal class SampleTests
+{
+    [TestFixture]
+    internal class ModelTests : UmtTests
+    {
+        [Test]
+        public async Task ImportService_SampleTest()
+        {
+            var root = RootNode;
+            root.NodeGUID.Should().Be(RootNodeGuid);
+
+            1.Should().NotBe(2);
+            1.Should().Be(1);
+
+            var importService = ServiceProvider.GetRequiredService<IImportService>();
+
+            // create observer to track import state
+            var importObserver = new ImportStateObserver();
+
+            // listen to validation errors
+            importObserver.ValidationError += (model, uniqueId, errors) =>
+            {
+                Console.WriteLine($"Validation error '{uniqueId}': {JsonSerializer.Serialize(errors)}");
+            };
+
+            var imported = new List<BaseInfo>();
+            // listen to successfully adapted and persisted objects
+            importObserver.ImportedInfo += info =>
+            {
+                imported.Add(info);
+                if (info is TreeNode tn)
+                {
+                    Console.WriteLine($"Imported node: {tn.NodeAlias}");
+                }
+                else
+                {
+                    Console.WriteLine($"Imported: {info[info.TypeInfo.CodeNameColumn]}");
+                }
+            };
+
+            // listen for exception occurence
+            importObserver.Exception += (model, uniqueId, exception) =>
+            {
+                Console.WriteLine($"Error: '{uniqueId}': {exception}");
+            };
+
+            // sample data
+            var sourceData = new UmtModel[]
+            {
+                // TODO: use your data
+                UserSamples.FreddyAdministrator, DataClassSamples.ArticleClassSample, DataClassSamples.EventDataClass, TreeNodeSamples.YearlyEvent, TreeNodeSamples.SingleOccurenceEvent,
+            };
+
+            // fill context
+            var context = new ImporterContext(
+                // TODO: change site name
+                "Boilerplate",
+                // TODO: change culture if needed
+                "en-US"
+            );
+
+            // initiate import
+            var observer = importService.StartImport(sourceData, context, importObserver);
+
+            // wait until import finishes
+            await observer.ImportCompletedTask;
+
+            imported.Should().SatisfyRespectively(
+                user =>
+                {
+                },
+                articleClass =>
+                {
+                },
+                eventClass =>
+                {
+                },
+                eventInstance =>
+                {
+                },
+                eventInstance =>
+                {
+                }
+            );
+
+#pragma warning restore S1135
+        }
+    }
+}
---
+++ Tests/Kentico.UMT.Tests/UmtTests.cs
@@ -0,0 +1,73 @@
+﻿using CMS.Core;
+using CMS.DataEngine;
+using CMS.DocumentEngine;
+using CMS.SiteProvider;
+using CMS.Tests;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Tests.DocumentEngine;
+
+namespace Kentico.Xperience.UMT;
+
+internal abstract class UmtTests : UnitTests
+{
+    public const string FakeSiteName = "Boilerplate";
+    public const string FakeCulture = "en-US";
+    public static readonly Guid RootNodeGuid = TreeNodeSamples.SiteRootNodeGuid;
+    
+    private static int nodeCount = 0;
+    
+    protected IServiceCollection Services = null!;
+    protected IServiceProvider ServiceProvider = null!;
+
+    private static TreeNode? rootNode;
+    public static TreeNode RootNode => rootNode ??= CreateNode("/", "CMS.Root", RootNodeGuid);
+
+    [SetUp]
+    public void SetUp()
+    {
+        Services = new ServiceCollection();
+        Services.AddLogging(b => b.AddDebug());
+        Services.AddUniversalMigrationToolkit();
+        ServiceProvider = Services.BuildServiceProvider();
+        
+        // Registers the page type class
+        // The 'CustomPageType' class is generated for a page type from the Xperience Page types application
+        DocumentGenerator.RegisterDocumentType<TreeNode>("CMS.Root");
+
+        // Fakes the page type to allow creation of fake page data
+        Fake().DocumentType<TreeNode>("CMS.Root");
+        
+        Fake<SiteInfo, SiteInfoProvider>().WithData(
+            new SiteInfo { SiteName = FakeSiteName }
+        );
+    }
+    
+    
+    private static TreeNode CreateNode(string nodeAliasPath, string contentType, Guid nodeGuid, string culture = FakeCulture, string site = FakeSiteName)
+    {
+        nodeCount++;
+        var nodeSite = SiteInfo.Provider.Get(site);
+        var node = TreeNode.New(contentType).With(p =>
+        {
+            p.DocumentCulture = culture;
+            p.DocumentName = Guid.NewGuid().ToString();
+            p.SetValue(nameof(TreeNode.DocumentID), nodeCount);
+            p.SetValue(nameof(TreeNode.NodeGUID), nodeGuid);
+            p.SetValue(nameof(TreeNode.NodeSiteID), nodeSite.SiteID);
+            p.SetValue(nameof(TreeNode.DocumentCreatedWhen), new DateTime(2022, 1, 1));
+            p.SetValue(nameof(TreeNode.NodeAliasPath), nodeAliasPath);
+        });
+
+        return node;
+    }
+
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Method intentionally left empty.
+    }
+}
+
