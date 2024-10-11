460b50ce507584070c367324428ac2c59ece715a
akfakmot <akfakmot@gmail.com>
2024-10-11 11:02:58 +0200

    Remove commented code
    Chore: Remove commented code

--- src/Kentico.Xperience.UMT/InfoAdapter/MemberAdapter.cs
+++ src/Kentico.Xperience.UMT/InfoAdapter/MemberAdapter.cs
@@ -8,16 +8,4 @@ internal class MemberAdapter : GenericInfoAdapter<MemberInfo>
     internal MemberAdapter(ILogger<MemberAdapter> logger, GenericInfoAdapterContext context) : base(logger, context)
     {
     }
-
-    //protected override void SetValue(UserInfo current, string propertyName, object? value)
-    //{
-    //    if (propertyName == "UserPassword")
-    //    {
-    //        current.SetValue("UserPassword", value);
-    //    }
-    //    else
-    //    {
-    //        base.SetValue(current, propertyName, value);
-    //    }
-    //}
 }
