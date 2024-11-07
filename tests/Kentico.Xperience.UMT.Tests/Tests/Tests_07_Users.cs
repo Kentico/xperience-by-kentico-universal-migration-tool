15e3bfa78e4155e55d32700d375c44784af15897
Martin Medek <MartinM4@kentico.com>
2024-11-07 14:15:32 +0100

    commented out most of tests for faster development of pipeline

--- tests/Kentico.Xperience.UMT.Tests/Tests/Tests_07_Users.cs
+++ tests/Kentico.Xperience.UMT.Tests/Tests/Tests_07_Users.cs
@@ -4,15 +4,15 @@ namespace TestAfterMigration.Tests
 {
     public class Tests_07_Users : AdminTestBase
     {
-        [Test]
-        public async Task Test00100_sadmin_Explorable_Without_Errors()
-        {
-            await OpenAdminApplication("Users");
+        // [Test]
+        // public async Task Test00100_sadmin_Explorable_Without_Errors()
+        // {
+        //     await OpenAdminApplication("Users");
 
-            await Page.GetByTestId("table-cell-UserName").Filter(new LocatorFilterOptions { HasText = "sadmin" }).Nth(0).ClickAsync();
-            await Debounce();
+        //     await Page.GetByTestId("table-cell-UserName").Filter(new LocatorFilterOptions { HasText = "sadmin" }).Nth(0).ClickAsync();
+        //     await Debounce();
 
-            await AssertNoEventlogErrors();
-        }
+        //     await AssertNoEventlogErrors();
+        // }
     }
 }
