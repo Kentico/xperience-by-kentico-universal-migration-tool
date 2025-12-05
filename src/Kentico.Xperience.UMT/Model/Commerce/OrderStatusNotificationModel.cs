cfd17a747b700949a7b3155a78c3eeb0ca2f5a93
Peter Petrinec <PeterP@kentico.com>
2025-12-05 16:19:36 +0100

    KX-21560 Fix OrderStatusNotificationModel setup

--- src/Kentico.Xperience.UMT/Model/Commerce/OrderStatusNotificationModel.cs
+++ src/Kentico.Xperience.UMT/Model/Commerce/OrderStatusNotificationModel.cs
@@ -16,18 +16,15 @@ public class OrderStatusNotificationModel : UmtModel
 {
     public const string DISCRIMINATOR = "OrderStatusNotification";
 
-    [Map]
-    [Required]
-    [UniqueIdProperty]
-    public Guid? OrderStatusNotificationGUID { get; set; }
-
     [Required]
     [ReferenceProperty(typeof(OrderStatusInfo), "OrderStatusNotificationOrderStatusID", IsRequired = true)]
+    [UniqueKeyPart("OrderStatusNotificationOrderStatusID", typeof(OrderStatusInfo))]
     public Guid? OrderStatusNotificationOrderStatusGUID { get; set; }
 
     [Required]
     [ReferenceProperty(typeof(UserInfo), "OrderStatusNotificationUserID", IsRequired = true)]
+    [UniqueKeyPart("OrderStatusNotificationUserID", typeof(UserInfo))]
     public Guid? OrderStatusNotificationUserGUID { get; set; }
 
-    protected override (Guid? uniqueId, string? name, string? displayName) GetPrintArgs() => (OrderStatusNotificationGUID, NOT_AVAILABLE, NOT_AVAILABLE);
+    protected override (Guid? uniqueId, string? name, string? displayName) GetPrintArgs() => (OrderStatusNotificationOrderStatusGUID, NOT_AVAILABLE, NOT_AVAILABLE);
 }
