ff3a24b9c65fd23c99cc005ea40db4b26fc9a4a8
michaljakubis <michalj@kentico.com>
2026-01-07 08:27:50 +0100

    Suppress sonar lint warning

--- src/Kentico.Xperience.UMT/InfoAdapter/PromotionAdapter.cs
+++ src/Kentico.Xperience.UMT/InfoAdapter/PromotionAdapter.cs
@@ -17,9 +17,11 @@ internal class PromotionAdapter : GenericInfoAdapter<PromotionInfo>
         if (propertyName == nameof(Model.PromotionModel.PromotionRuleConfiguration) && value is string ruleConfiguration)
         {
             // Use reflection to set the internal PromotionRuleConfiguration property
+#pragma warning disable S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
             var propertyInfo = typeof(PromotionInfo).GetProperty(
                 nameof(Model.PromotionModel.PromotionRuleConfiguration),
                 BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+#pragma warning restore S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
 
             if (propertyInfo != null)
             {
